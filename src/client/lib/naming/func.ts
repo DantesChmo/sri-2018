4c3aaeb54d938d18c24f037bda6e8bc66ecf1d21
dimalemur <dim4ik120899@gmail.com>
2019-09-23 19:22:52 +0300

    fixed the bem-function

--- src/client/lib/naming/func.ts
+++ src/client/lib/naming/func.ts
@@ -1,8 +1,8 @@
-interface modifierValueTypeIsObject{
-    [key:string]:string|boolean;
+interface ModifierValueTypeIsObject{
+    [key: string]: string|boolean;
 }
-interface typeOfModificationForDeclorationBEM {
-    [key:string]:modifierValueTypeIsObject|string;
+interface TypeOfModificationForDeclorationBEM {
+    [key: string]: ModifierValueTypeIsObject|string;
 }
 
 enum Mode {
@@ -10,71 +10,69 @@ enum Mode {
     REACT
 }
 
-function nameFormat(string:string):string{
-        let result:string = "";
-        let lastChar:string = "";
-        for (const char of string) {
-            if(lastChar === " " || lastChar === "-" || lastChar == ""){
-                result += char.toUpperCase();
-            }else {
-                result +=char.toLowerCase();
-            }
-            lastChar = char;
-        }
-
-    return result
-}
-
-function nameLowerCase(string:string):string{
-    let result:string = "";
+function nameFormat(string: string): string {
+    let result = '';
+    let lastChar = '';
     for (const char of string) {
-        result += char.toLowerCase()        
+        if (lastChar === ' ' || lastChar === '-' || lastChar === '') {
+            result += char.toUpperCase();
+        } else {
+            result += char.toLowerCase();
+        }
+        lastChar = char;
     }
-    return result
+
+    return result;
 }
 
-function checkValue(obj:modifierValueTypeIsObject,modSeparator:string,currentKey:string):string{
-    let result:string;
-    if (typeof(obj[currentKey]) === "boolean" ){            
+function checkValue(obj: ModifierValueTypeIsObject, modSeparator: string, currentKey: string): string {
+    let result: string;
+    if (typeof (obj[currentKey]) === 'boolean' && obj[currentKey] === true) {
         result = modSeparator + currentKey;
     } else {
         result = modSeparator + currentKey + modSeparator + obj[currentKey];
     }
-    return result
+    return result;
 }
- 
-function declorationBEM(mode:Mode, elemSeparator:string, modSeparator:string){
-    return function bemGenerator(block:string, elem:string = "", mod?:typeOfModificationForDeclorationBEM|modifierValueTypeIsObject){
 
-        block = nameLowerCase(block);
-        elem = nameLowerCase(elem);
-        let result:string = "";
-        result +=block;
-        if (elem != ""){
-            result += ` ${elem + elemSeparator + block}`
+function declorationBEM(mode: Mode, elemSeparator: string, modSeparator: string) {
+    return function bemGenerator(block: string, elem = '', mod?: TypeOfModificationForDeclorationBEM|ModifierValueTypeIsObject) {
+        block = block.toLowerCase();
+        elem = elem.toLowerCase();
+        let result = '';
+        let blockModifier = '';
+        let elemModifier = '';
+
+        result += block;
+        if (elem !== '') {
+            result += ` ${elem + elemSeparator + block}`;
         }
-        if (mod != undefined && !Array.isArray(mod)){
+        if (mod !== undefined && !Array.isArray(mod)) {
+            Object.keys(mod).forEach((modKeys) => {
+                const modValue: any = mod[modKeys];
+                if (typeof (modValue) === 'object') {
+                    Object.keys(modValue).forEach((keyOfModValue) => {
+                        if (modKeys.toLowerCase() === block) {
+                            const blockModifierAdditive: string = ` ${modKeys + checkValue(modValue, modSeparator, keyOfModValue)}`.toLowerCase();
+                            if (blockModifier.match(blockModifierAdditive) === null) {
+                                blockModifier += blockModifierAdditive;
+                            }
+                        } else if (modKeys.toLowerCase() === elem) {
+                            const elemModifierAdditive: string = ` ${modKeys + elemSeparator + block + checkValue(modValue, modSeparator, keyOfModValue)}`.toLowerCase();
 
-            Object.keys(mod).forEach(function(modKeys){
-                const modValue:any = mod[modKeys];               
-                if (typeof(modValue) === "object"){
-                    Object.keys(modValue).forEach(function(keyOfModValue){ //по внутреннему объекту
-                        if (nameLowerCase(modKeys) === block){
-                            result += ` ${modKeys + checkValue(modValue,modSeparator,keyOfModValue)}`;
-                        } else if (nameLowerCase(modKeys) === elem) {
-                            result += ` ${modKeys + elemSeparator + block + checkValue(modValue,modSeparator,keyOfModValue)}`;                                
+                            if (elemModifier.match(elemModifierAdditive) === null) {
+                                elemModifier += elemModifierAdditive;
+                            }
                         }
                     });
                 } else {
-                    const defaultObject:any = mod;
-                    result += ` ${block + checkValue(defaultObject,modSeparator,modKeys)}`;
+                    const defaultObject: any = mod;
+                    result += ` ${block + checkValue(defaultObject, modSeparator, modKeys)}`;
                 }
             });
-
-
         }
-        return nameFormat(result)
-    }
+        return nameFormat(result + blockModifier + elemModifier);
+    };
 }
 
-export const bem = declorationBEM(Mode.REACT,"-","_");
+export const bem = declorationBEM(Mode.REACT, '-', '_');
